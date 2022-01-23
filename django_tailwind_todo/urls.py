14466acf970924100932e9d9c59d3836810f4842
Stein Ove Helset <steinovehelset@gmail.com>
2022-01-23 17:11:53 +0100

    part 2

--- django_tailwind_todo/urls.py
+++ django_tailwind_todo/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from todo.views import todos, add_todo, update_todo, delete_todo
+from todo.views import todos, add_todo, update_todo, delete_todo, edit_todo
 
 urlpatterns = [
     path('', todos, name='todos'),
     path('add-todo/', add_todo, name='add_todo'),
     path('update/<int:pk>/', update_todo, name='update_todo'),
     path('delete/<int:pk>/', delete_todo, name='delete_todo'),
+    path('edit/<int:pk>/', edit_todo, name='edit_todo'),
     path('admin/', admin.site.urls),
 ]
--- todo/views.py
+++ todo/views.py
@@ -9,6 +9,18 @@ def todos(request):
 
     return render(request, 'todo/todos.html', {'todos': todos})
 
+@require_http_methods(['GET', 'POST'])
+def edit_todo(request, pk):
+    todo = Todo.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        todo.title = request.POST.get('title', '')
+        todo.save()
+
+        return render(request, 'todo/partials/todo.html', {'todo': todo})
+    
+    return render(request, 'todo/partials/edit.html', {'todo': todo})
+
 @require_http_methods(['POST'])
 def add_todo(request):
     todo = None
